239fe86cc07132f5c29d5dcd5130007fbcb32ff5
DevashishDas3 <dedas2024@gmail.com>
2023-07-24 14:34:09 -0400

    Import Classes

--- chat_client.py
+++ chat_client.py
@@ -1,11 +1,19 @@
 import socket
 import datetime
 
+import server
+import client
+import message
+import chatroom
+
+
+
 PORT = 8050
 HOST = "0.0.0.0"
 
 #IP: 10.29.48.1
 
+messages = []
 
 def init_connections(HOST, PORT, num_conns):
     address = (HOST, PORT)
@@ -13,10 +21,22 @@ def init_connections(HOST, PORT, num_conns):
         connid += 1
         print(f'Connecting ID {connid} to {address}')
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.setblocking(False)
         server.connect_ex(HOST, PORT)
+        server.setblocking(False)
         while True:
-            data = server.recv(1024)
-            server.sendall()
+            try:
+                data = server.recv(1024)
+                data.decode("UTF-8")
+                messages.append(data)
+                for i in range(len(messages)):
+                    print(messages[i], "\n")
+            except :
+                print("Error")
+                server.close()
+            
+
+            
+
+        
         
 
