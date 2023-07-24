37edab2e710d02c5f20c40ea1d2aed1c89f74975
RomeDevelopment <rome.henry.bowman@gmail.com>
2023-07-24 14:47:31 -0400

    added stuff

--- chat_server.py
+++ chat_server.py
@@ -1,12 +1,20 @@
 import socket
 ##import datetime
 import requirements.txt
+from "client.py" import Client
 
 HOST=socket.gethostbyname(socket.gethostname())
 
 
 PORT=8050
 
+Dev_client
+Rome_client
+Arthur_client
+Client Jules_client()
+
+
+client_list=[]
 
 if __name__ == "__main__":
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -23,3 +31,8 @@ if __name__ == "__main__":
                     break
                 else:
                     print(data.strip())
+
+
+
+##Need function that takes in new client's info and adds them to the system
+
