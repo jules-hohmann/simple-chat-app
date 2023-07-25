f8ce375c1d8eb818f10865777ed6f956b62373b0
RomeDevelopment <rome.henry.bowman@gmail.com>
2023-07-25 09:12:29 -0400

    Made working server/client

--- Fake_server.py
+++ Fake_server.py
@@ -2,11 +2,12 @@ import socket
 import threading
 
 
+##SERVER=socket.gethostbyname(socket.getfqdn(socket.gethostname()))
 
-SERVER=socket.gethostbyname(socket.getfqdn(socket.gethostname()))
-PORT = 8005
-
-
+SERVER="10.29.58.7"
+PORT = 8006
+HEADER=16
+FORMAT="UTF-8"
 server= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.bind((SERVER,PORT))
 
@@ -20,12 +21,17 @@ def handle_client(conn,addr):
 
     connected=True
     while connected:
-        message=conn.recv(1024)
-        while i <= 100:
-            print(f"[{addr}][{i}] {message}")
-            i+=1
-    if message==DISCONNECT_MESSAGE:
-        connected=False
+        msg_length=conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length=int(msg_length)
+            message=conn.recv(msg_length).decode(FORMAT)
+            message=message.strip()
+            if message==DISCONNECT_MESSAGE:
+                connected=False
+            if i <= 100:
+                print(f"[{addr}][{i}] {message}")
+                i+=1
+    conn.close()
 
 
 def start():
@@ -36,3 +42,8 @@ def start():
         thread= threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
         print(f"[ACTIVE CONNECTIONS] {threading.activeCount()-1}")
+
+
+if __name__=="__main__":
+    start()
+    
